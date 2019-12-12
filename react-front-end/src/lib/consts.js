2613b1b1ffc7b31313ded8607e6c5190ffc56a0d
carneirofc <claudiofcarneiro@hotmail.com>
2019-12-12 11:05:40 -0300

    interface: public ip.

--- react-front-end/src/lib/consts.js
+++ react-front-end/src/lib/consts.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-export const WEBDIS_URL = "http://10.128.255.4:7379";
+export const WEBDIS_URL = "http://10.0.38.59:7379";
 export const GET = WEBDIS_URL + '/GET';
 
 export const PVS = [
