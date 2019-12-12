f21e9018209d784f88538fd8edaf30d11fde6573
carneirofc <claudiofcarneiro@hotmail.com>
2019-12-12 10:29:36 -0300

    Update webdis url.

--- react-front-end/src/lib/consts.js
+++ react-front-end/src/lib/consts.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-export const WEBDIS_URL = "http://10.0.6.48:7379";;
+export const WEBDIS_URL = "http://10.128.255.5/webdis";
 export const GET = WEBDIS_URL + '/GET';
 
 export const PVS = [
